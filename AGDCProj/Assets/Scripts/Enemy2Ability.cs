ba001c307b9d3464ffd83b16a0ba60708cdcbc12
DZsuedonimm <jawadusman@ymail.com>
2019-02-09 15:00:07 -0500

    Ability "Steal" System

---
+++ AGDCProj/Assets/Scripts/Enemy2Ability.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2Ability : MonoBehaviour
+{
+    public GameObject proj;
+    public Transform projSpawn;
+
+    public bool ability2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ability2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ability2 == true)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(proj, projSpawn.position, projSpawn.rotation);
+
+            }
+        }
+    }
+}
