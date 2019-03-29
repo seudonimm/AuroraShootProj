ffa62551a2530e0cd2452b5d5b0111186f1656f7
DZsuedonimm <jawadusman@ymail.com>
2019-03-29 02:56:12 -0400

    Checkpoint#3
    - 4/5 bullets added(laser not yet)
    -

---
+++ AGDCProj/Assets/Scripts/HomingBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingBullet : MonoBehaviour {
+
+    public Transform target;
+
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        //Locates the Player
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //The direction from the missle to the target.
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, -transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = -transform.up * speed;
+	}
+}
